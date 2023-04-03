79aef628d8e40f9f5aab60f58e5a494d6f4dd58a
Adrian <adrianalexander17@gmail.com>
2023-04-03 16:12:53 -0400

    [chore] connected UI buttons and values to backend

--- HeartWave/mainwindow.cpp
+++ HeartWave/mainwindow.cpp
@@ -2,19 +2,54 @@
 #include "ui_mainwindow.h"
 using namespace std;
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+//Global variables
+
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
 
     consoleMenu();
+    initialize();
     ui->setupUi(this);
+
+    //Directions
+    connect(ui->buttonUp, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonDown, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonLeft, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonRight, &QPushButton::clicked, this, &MainWindow::handleButtons);
+
+    //Other
+    connect(ui->buttonOk, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonCharge, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonHeart, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonMenu, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonPower, &QPushButton::clicked, this, &MainWindow::handleButtons);
+    connect(ui->buttonBack, &QPushButton::clicked, this, &MainWindow::handleButtons);
 
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initialize() {
+    qDebug() << qPrintable("======Initializing Button Mapping======");
+
+    mapStringValues["buttonUp"] = stringValue::up;
+    mapStringValues["buttonDown"] = stringValue::down;
+    mapStringValues["buttonLeft"] = stringValue::left;
+    mapStringValues["buttonRight"] = stringValue::right;
+    mapStringValues["buttonOk"] = stringValue::ok;
+    mapStringValues["buttonCharge"] = stringValue::charge;
+    mapStringValues["buttonHeart"] = stringValue::heart;
+    mapStringValues["buttonMenu"] = stringValue::menu;
+    mapStringValues["buttonPower"] = stringValue::power;
+    mapStringValues["buttonBack"] = stringValue::back;
+
+    qDebug() << qPrintable("mapStringValues contains " + QString::number(mapStringValues.size()) + " entries.");
+
+    qDebug() << qPrintable("======Initializing Value Mapping======");
+
 }
 
 //Function to test the menu through the console until front-enders connect the UI to our functions...
@@ -42,3 +77,113 @@ void MainWindow::consoleMenu() {
     qDebug() << qPrintable("Tuple Br " + QString::number(br));
     qDebug() << qPrintable("Tuple CL " + QString::number(cl));
 }
+
+void MainWindow::handleButtons() {
+    //Get button
+    QPushButton* buttonSender = qobject_cast<QPushButton*>(sender());
+
+    QString buttonName = buttonSender->objectName();
+
+    switch(mapStringValues[buttonName.toStdString()]) {
+        case stringValue::up:
+            qDebug() << qPrintable("Up function");
+//            setBattery(69);
+//            ui->buttonCoherenceLow->setChecked(true);
+            setLow_UI(true);
+        break;
+        case stringValue::down:
+            qDebug() << qPrintable("Down function");
+            setMed_UI(true);
+        break;
+        case stringValue::left:
+            qDebug() << qPrintable("Left function");
+            setHigh_UI(true);
+        break;
+        case stringValue::right:
+            qDebug() << qPrintable("Right function");
+            setHigh_UI(false);
+        break;
+        case stringValue::ok:
+            qDebug() << qPrintable("Ok function");
+        break;
+        case stringValue::charge:
+            qDebug() << qPrintable("Charge funtion");
+        break;
+        case stringValue::heart:
+            qDebug() << qPrintable("Heart funtion");
+        break;
+        case stringValue::menu:
+            qDebug() << qPrintable("Menu funtion");
+        break;
+        case stringValue::power:
+            qDebug() << qPrintable("Power funtion");
+        break;
+        case stringValue::back:
+            qDebug() << qPrintable("Back funtion");
+        break;
+    }
+
+}
+
+void MainWindow::setBattery_UI(int newVal) {
+    ui->progressBatteryLevel->setValue(newVal);
+}
+
+//void MainWindow::setCharge_UI(int newVal) {
+//    ui->
+//}
+
+//void MainWindow::setContact_UI(bool newVal) {
+//    ui->
+//}
+
+void MainWindow::setLength_UI(int newVal) {
+    ui->textLength->setText(QString::number(newVal));
+}
+
+void MainWindow::setHC_UI(int newVal) {
+    ui->textCoherence->setText(QString::number(newVal));
+}
+
+void MainWindow::setLow_UI(bool newVal) {
+    if (newVal) {
+        ui->buttonCoherenceLow->setStyleSheet(
+            "background-color:rgb(255,0,0);"
+            "color:rgb(255,255,255);"
+        );
+    } else {
+        ui->buttonCoherenceLow->setStyleSheet(
+            "background-color:rgb(125,0,0);"
+            "color:rgb(255,255,255);"
+        );
+    }
+}
+
+void MainWindow::setMed_UI(bool newVal) {
+    if (newVal) {
+        ui->buttonCoherenceMedium->setStyleSheet(
+            "background-color:rgb(0,0,255);"
+            "color:rgb(255,255,255);"
+        );
+    } else {
+        ui->buttonCoherenceMedium->setStyleSheet(
+            "background-color:rgb(0,0,125);"
+            "color:rgb(255,255,255);"
+        );
+    }
+}
+
+void MainWindow::setHigh_UI(bool newVal) {
+    if (newVal) {
+        ui->buttonCoherenceHigh->setStyleSheet(
+            "background-color:rgb(0,255,0);"
+            "color:rgb(255,255,255);"
+        );
+    } else {
+        ui->buttonCoherenceHigh->setStyleSheet(
+            "background-color:rgb(0,125,0);"
+            "color:rgb(255,255,255);"
+        );
+    }
+}
+
--- HeartWave/mainwindow.h
+++ HeartWave/mainwindow.h
@@ -18,9 +18,41 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     void consoleMenu();
+    void handleButtons();
     ~MainWindow();
+
+    //setters for UI values, not all of them will be useful (sessionID)
+    void setBattery_UI(int newVal); //done
+    //whats the difference between battery and charge
+    void setCharge_UI(bool newVal);
+    void setContact_UI(bool newVal);
+    void setLength_UI(int newVal); //done
+    void setID_UI(int newVal);
+    void setHC_UI(int newVal); //done
+    void setCS_UI(int newVal);
+    void setCL_UI(int newVal);
+    void setHRV_UI(int newVal);
+
+    void setLow_UI(bool newVal);
+    void setMed_UI(bool newVal);
+    void setHigh_UI(bool newVal);
 
 private:
     Ui::MainWindow *ui;
+    enum stringValue {
+        up,
+        down,
+        left,
+        right,
+        ok,
+        charge,
+        heart,
+        menu,
+        power,
+        back
+    };
+    std::map<std::string, stringValue> mapStringValues;
+    void initialize();
+
 };
 #endif // MAINWINDOW_H
